98f619d67b28a5f5cf9a0e0edb1641822115b203
932441969 <932441969@qq.com>
2025-04-26 12:48:07 +0800

    Site updated: 2025-04-26 12:48:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/25/hello-world/"];function toRandomPost(){
+var posts=["2025/04/25/hello-world/","2025/04/26/when-I-dont-sleep/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
